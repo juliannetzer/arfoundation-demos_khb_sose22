d91fe35ff913d4e57bae6f5d9fa2f708aa50ab7f
juliannetzer <102752277+juliannetzer@users.noreply.github.com>
2022-04-27 14:49:55 +0200

    Added demo scenes

---
+++ Assets/01_Demo_Scenes/02_ImageTracking/Assets/Scripts/HideObjectOnLostTracker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+namespace UnityEngine.XR.ARFoundation.Samples
+{
+    /// This component listens for images detected by the <c>XRImageTrackingSubsystem</c>
+    /// and overlays some information as well as the source Texture2D on top of the
+    /// detected image.
+    /// </summary>
+    [RequireComponent(typeof(ARTrackedImageManager))]
+	public class HideObjectOnLostTracker : MonoBehaviour
+    {
+        
+
+        ARTrackedImageManager m_TrackedImageManager;
+
+        void Awake()
+        {
+            m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+        }
+
+        void OnEnable()
+        {
+            m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        }
+
+        void OnDisable()
+        {
+            m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+        }
+
+        void UpdateInfo(ARTrackedImage trackedImage)
+        {
+
+            // Disable the visual plane if it is not being tracked
+	        if (trackedImage.trackingState != TrackingState.None && trackedImage.trackingState != TrackingState.Limited)
+            {
+	            trackedImage.gameObject.SetActive(true);
+	            // planeGo.SetActive(true);
+
+                // // The image extents is only valid when the image is being tracked
+                // trackedImage.transform.localScale = new Vector3(trackedImage.size.x, 1f, trackedImage.size.y);
+
+                // // Set the texture
+                // var material = planeGo.GetComponentInChildren<MeshRenderer>().material;
+                // material.mainTexture = (trackedImage.referenceImage.texture == null) ? defaultTexture : trackedImage.referenceImage.texture;
+            }
+            else
+            {
+	            trackedImage.gameObject.SetActive(false);
+
+	            // planeGo.SetActive(false);
+            }
+        }
+
+        void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+        {
+            foreach (var trackedImage in eventArgs.added)
+            {
+                // Give the initial image a reasonable default scale
+                trackedImage.transform.localScale = new Vector3(0.01f, 1f, 0.01f);
+
+                UpdateInfo(trackedImage);
+            }
+
+            foreach (var trackedImage in eventArgs.updated)
+                UpdateInfo(trackedImage);
+        }
+    }
+}
