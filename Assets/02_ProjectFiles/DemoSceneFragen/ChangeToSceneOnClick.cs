c22caa37b73f281dd9c69dbb51c7a1cea3474f8f
juliannetzer <102752277+juliannetzer@users.noreply.github.com>
2022-05-23 16:56:19 +0200

    Added Example Scene

---
+++ Assets/02_ProjectFiles/DemoSceneFragen/ChangeToSceneOnClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeToSceneOnClick : MonoBehaviour
+{
+    
+	private Vector2 touchPosition = default;
+	//public GameObject Maus; 
+	
+	[Header("Allows you to jump to a scene when clicking on an object in AR")] 
+	[Tooltip("Name of the Scene to jump to ")]
+	[SerializeField]
+	private string SceneName;
+ 
+	private void Start() {
+			}
+ 
+	void Update() {
+		/*if (Input.GetKeyDown(KeyCode.Space)){
+			Maus.GetComponent<Animator>().SetTrigger("Running");
+		}*/
+
+		if(Input.touchCount > 0) {
+			Touch touch = Input.GetTouch(0);
+			touchPosition = touch.position;
+ 
+ 
+				if (touch.phase == TouchPhase.Began) {
+					Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+					RaycastHit hitObject;
+ 
+					if (Physics.Raycast(ray, out hitObject)) {
+ 
+						if(hitObject.transform.CompareTag(this.gameObject.tag)){
+							SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+
+						}
+					}
+				}
+			
+		}
+	}
+ 
+}
---
+++ Assets/02_ProjectFiles/DemoSceneFragen/SelectRandomObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectRandomCharacter : MonoBehaviour
+{
+	
+	public GameObject[] Characters; 
+	void Awake()
+    {
+	    Characters = new GameObject[transform.childCount];
+	    int children = transform.childCount;
+	    for (int i = 0; i < children; ++i){
+		    Characters[i] = transform.GetChild(i).gameObject;
+	    }
+	    int ranCharacter = Random.Range(0,children);
+	    Characters[ranCharacter].SetActive(true);
+    }
+}
+    
